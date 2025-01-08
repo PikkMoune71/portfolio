25f65ef66f51c5293e7353d8eb58e21a9a9fab6f
PikkMoune71 <antoinepicard385@gmail.com>
2025-01-08 16:32:48 +0100

    fix: title center

--- components/Maintenance.tsx
+++ components/Maintenance.tsx
@@ -7,7 +7,7 @@ export default function Maintenance() {
         "flex flex-col items-center justify-center min-h-screen bg-gray-100"
       )}
     >
-      <h1 className="text-4xl font-bold text-gray-800">
+      <h1 className="text-4xl font-bold text-gray-800 text-center">
         Site en cours de construction 🚧
       </h1>
       <p className="text-lg text-gray-600 mt-4 text-center">
