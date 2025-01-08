9673ac85f452e172773724a7c61770a1c9154bca
PikkMoune71 <antoinepicard385@gmail.com>
2025-01-08 16:36:26 +0100

    feat: import mainteance.tsx in dev

---
+++ components/Maintenance.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+export default function Maintenance() {
+  return (
+    <div
+      className={cn(
+        "flex flex-col items-center justify-center min-h-screen bg-gray-100"
+      )}
+    >
+      <h1 className="text-4xl font-bold text-gray-800 text-center">
+        Site en cours de construction 🚧
+      </h1>
+      <p className="text-lg text-gray-600 mt-4 text-center">
+        Nous travaillons dur pour vous offrir une expérience incroyable. Revenez
+        bientôt !
+      </p>
+    </div>
+  );
+}
