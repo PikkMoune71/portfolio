01996e67667123c966d232f4b050aea2b18b2e38
PikkMoune71 <antoinepicard385@gmail.com>
2025-01-09 11:39:45 +0100

    feat: about components

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,13 @@
+import { About } from "@/components/About";
 import { Header } from "@/components/Header";
 import { ModeToggle } from "@/components/ModeToggle";
-
-import TextDescription from "@/components/TextDescription";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import * as motion from "framer-motion/client";
 
 export default function Home() {
   return (
     <>
       <Header />
       <div className="h-20"></div>
-      <div className="flex flex-col justify-center items-center">
-        <motion.div
-          initial={{ scale: 0 }}
-          animate={{ rotate: 0, scale: 1 }}
-          transition={{
-            type: "spring",
-            stiffness: 260,
-            damping: 20,
-          }}
-        >
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>AP</AvatarFallback>
-          </Avatar>
-        </motion.div>
-        <TextDescription />
-      </div>
-
+      <About />
       <div className="fixed bottom-10 right-10">
         <ModeToggle />
       </div>
