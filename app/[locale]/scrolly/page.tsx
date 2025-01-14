a8be2a3b66a4f0b54ae567080043cd1cf305a31b
PikkMoune71 <antoinepicard385@gmail.com>
2025-01-14 17:01:14 +0100

    feat: add page Scrolly project

--- app/[locale]/scrolly/page.tsx
+++ app/[locale]/scrolly/page.tsx
@@ -1,7 +1,160 @@
+"use client";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { useCurrentLocale, useScopedI18n } from "@/locales/client";
+import { useEffect, useState } from "react";
+import { typedSkillsScrolly, categories } from "@/data/skills";
+import { SkillCategory } from "@/components/SkillsCategory";
+import { openSauceBlack, animations } from "@/utils/constant";
+
 export default function Scrolly() {
+  const images = [
+    "/scrolly/scrolly-1.png",
+    "/scrolly/scrolly.png",
+    "/scrolly/scrolly-2.png",
+  ];
+  const t = useScopedI18n("scrolly");
+  const currentLanguage = useCurrentLocale();
+
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
   return (
-    <div>
-      <h1>Scrolly</h1>
+    <div className="m-auto py-8 px-6 max-w-5xl w-full relative">
+      <div className="mx-full">
+        <Carousel setApi={setApi} className="w-full">
+          <CarouselContent>
+            {images.map((src, index) => (
+              <CarouselItem key={index}>
+                <div className="">
+                  <Card>
+                    <CardContent className="flex items-center justify-center p-0">
+                      <motion.div
+                        initial={{ scale: 0.9 }}
+                        animate={{ scale: 1 }}
+                        transition={{ duration: 0.5 }}
+                      >
+                        <Image
+                          src={src}
+                          alt={`Carousel item ${index + 1}`}
+                          layout="responsive"
+                          width={1000}
+                          height={2000}
+                          className="rounded-xl"
+                        />
+                      </motion.div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        <div className="py-2 text-center text-sm text-muted-foreground">
+          Slide {current} of {count}
+        </div>
+      </div>
+
+      <div className="text-center mt-10">
+        <motion.h2
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ type: "spring", stiffness: 300, damping: 20 }}
+          className={`${openSauceBlack.className} text-6xl`}
+        >
+          Scrolly
+        </motion.h2>
+        <span className="text-lg text-muted-foreground">Janvier 2024</span>
+      </div>
+      <motion.h2
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ type: "spring", stiffness: 300, damping: 20 }}
+        className={`${openSauceBlack.className} text-4xl mt-10 mb-2`}
+      >
+        {t("context")}
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0, x: -20 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.2 }}
+        className="text-lg text-left"
+      >
+        {t("contextScrolly")}
+      </motion.p>
+      <motion.h2
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ type: "spring", stiffness: 300, damping: 20 }}
+        className={`${openSauceBlack.className} text-4xl mt-10 mb-2`}
+      >
+        {t("description")}
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0, x: -20 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.2 }}
+        className="text-lg"
+      >
+        {t("descriptionScrolly")}
+      </motion.p>
+      <motion.h2
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ type: "spring", stiffness: 300, damping: 20 }}
+        className={`${openSauceBlack.className} text-4xl mt-10 mb-2`}
+      >
+        {t("technologyUsed")}
+      </motion.h2>
+      <motion.div
+        variants={animations.container}
+        initial="hidden"
+        animate="visible"
+        className="grid grid-cols-1 sm:grid-cols-2 gap-8"
+      >
+        {categories
+          .map((category) => ({
+            category,
+            skills: typedSkillsScrolly.filter(
+              (skill) => skill.category === category
+            ),
+          }))
+          .filter(({ skills }) => skills.length > 0)
+          .map(({ category, skills }, index) => (
+            <SkillCategory
+              key={index}
+              category={category}
+              skills={skills}
+              currentLanguage={currentLanguage}
+              itemAnimation={animations.item}
+            />
+          ))}
+      </motion.div>
     </div>
   );
 }
--- locales/fr.ts
+++ locales/fr.ts
@@ -25,4 +25,13 @@ export default {
     description:
       "Des projets variés, réalisés avec passion et professionnalisme, qui illustrent mon savoir-faire et mes compétences techniques.",
   },
+  scrolly: {
+    context: "Context",
+    contextScrolly:
+      "Pour mon projet de fin d’études, j’ai co-développé Scrolly, une plateforme conçue par une équipe de quatre étudiants, dont moi, pour simplifier la recherche d’alternances et de stages. L’objectif était de centraliser et de rendre accessible un processus souvent complexe pour les étudiants, tout en facilitant le recrutement pour les entreprises. Ce projet m’a permis de mettre en œuvre mes compétences en front-end et back-end, tout en intégrant des fonctionnalités avancées pour offrir une expérience utilisateur fluide et intuitive.",
+    description: "Description",
+    descriptionScrolly:
+      "Scrolly est une Progressive Web App (PWA) révolutionnaire conçue pour simplifier la recherche et la candidature à des opportunités d'alternance et de stage pour les étudiants. Actuellement, ce processus est souvent complexe et chronophage, nécessitant de naviguer sur de multiples plateformes et canaux de communication. D'un autre côté, les entreprises rencontrent des difficultés à trouver des candidats correspondant à leurs besoins spécifiques parmi une multitude de candidatures non ciblées. Scrolly centralise et simplifie ce processus, offrant une plateforme intuitive et efficace. En digitalisant et en automatisant certaines étapes clés du recrutement, Scrolly vise à devenir un acteur majeur dans le domaine du recrutement en alternance et stage, facilitant la connexion entre étudiants et entreprises.",
+    technologyUsed: "Technologies utilisées",
+  },
 } as const;
